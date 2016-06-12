e237ee9fa8f69e5244e9359d6cedd4cf4765b763
Kosuke Koiwai <kk@kocko.org>
2016-06-13 07:03:40 +0900

    modified meteor code to return nodeinfo and contractinfo
    fixed so meteor won't throw errors when these info is not stored in mongo

--- TradeMatchingWeb/server/main.js
+++ TradeMatchingWeb/server/main.js
@@ -8,14 +8,25 @@ export const Contract = new Mongo.Collection('contract');
 Router.route('/nodeinfo', function () {
   var req = this.request;
   var res = this.response;
-  var enode = Enode.findOne().enode;
-  res.end(enode);
+  if (Contract.find().count()==0){
+    res.end("");
+  }
+  else{
+    var enode = Enode.findOne().enode;
+    res.end(enode);
+  }
 }, {where: 'server'});
 
 Router.route('/contractinfo', function () {
   var req = this.request;
   var res = this.response;
-  var contractaddress = Contract.findOne().address;
-  res.end(contractaddress);
+  if (Contract.find().count()==0){
+    res.end("");
+  }
+  else{
+    var contractaddress = Contract.findOne().address;
+    res.end(contractaddress);
+  }
 }, {where: 'server'});
+
 
